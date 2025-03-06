505f604235e659895dfbd8e09024830602385812
YuSeungBeom <ykseok518@naver.com>
2025-03-06 20:26:16 +0900

    ⚙️ FEAT: Configure Django settings for development environment

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "skillbridge",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -123,6 +125,8 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
