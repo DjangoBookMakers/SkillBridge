7102d165fc662f61419e98a4bbf5397ea7faf952
SeungminAhn <smahn9123@gmail.com>
2025-03-07 20:49:47 +0900

    ✨ FEAT:  메인화면 과정 인기순 정렬

--- courses/views.py
+++ courses/views.py
@@ -1,20 +1,20 @@
+from django.db.models import Avg
 from django.shortcuts import render
 from .models import Course
 
 
 def course_list(request):
-    # 인기 과정 (리뷰 평점 기준으로 상위 6개)
-    # popular_courses = Course.objects.all().order_by('-reviews__rating')[:6]
-
-    # 중복 제거를 위한 처리 (같은 과정이 여러 리뷰를 받은 경우)
-    # popular_courses = list(dict.fromkeys(popular_courses))
+    # 인기 과정 (각 과정의 평균 평점으로 정렬)
+    # 평점이 동일하거나 NULL인 경우, 최신 과정이 먼저 표시
+    popular_courses = Course.objects.annotate(
+        avg_rating=Avg("reviews__rating")
+    ).order_by("-avg_rating", "-created_at")[:6]
 
     # 전체 과정 (최신순)
     all_courses = Course.objects.all().order_by("-created_at")
 
     context = {
-        # 'popular_courses': popular_courses,
-        "popular_courses": [],
+        "popular_courses": popular_courses,
         "all_courses": all_courses,
     }
 
