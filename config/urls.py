4b791824e48587798feb75bdfdf420308c3d4ee7
SeungminAhn <smahn9123@gmail.com>
2025-03-07 17:55:04 +0900

    Revert "✨ FEAT: courses app 추가"
    This reverts commit 6a88a6fb81fb4cbe0730128a09aac470fd5a0ffb.

--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -22,8 +21,7 @@ from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("accounts/", include("accounts.urls")),
-    path("courses/", include("courses.urls")),
-    path("", TemplateView.as_view(template_name="base.html"), name="home"),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('', TemplateView.as_view(template_name='base.html'), name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
