4b6d096e1a73f34cf324628b8656d76610cb2c93
YuSeungBeom <ykseok518@naver.com>
2025-03-07 10:01:40 +0900

    ✨ FEAT: Implement user authentication system with login and signup features

---
+++ accounts/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '아이디'}),
+        label='아이디'
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '비밀번호'}),
+        label='비밀번호'
+    )
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': '이메일'}),
+        required=True
+    )
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2', 'first_name', 
+                  'last_name', 'profile_image', 'phone_number', 'birth_date', 'gender']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '아이디'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '이름'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '성'}),
+            'phone_number': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '전화번호'}),
+            'birth_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'gender': forms.Select(attrs={'class': 'form-control'}, choices=[('', '성별 선택'), ('남성', '남성'), ('여성', '여성')]),
+            'profile_image': forms.FileInput(attrs={'class': 'form-control'}),
+        }
--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "skillbridge",
     "accounts",
 ]
 
@@ -126,7 +125,10 @@ STATICFILES_DIRS = [
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
