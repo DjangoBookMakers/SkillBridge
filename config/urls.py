98a058d8ea34c1dc2518bf220f406cbd14a04a07
SeungminAhn <smahn9123@gmail.com>
2025-03-08 18:55:13 +0900

    🐛 FIX: 메인 로고 클릭시 메인 과정 목록 화면으로 이동

--- config/urls.py
+++ config/urls.py
@@ -19,11 +19,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("courses/", include("courses.urls")),
-    path("", TemplateView.as_view(template_name="base.html"), name="home"),
+    path("", include("courses.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
