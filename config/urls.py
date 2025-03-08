d496d4cbc760688936b15a0e6a8d003b889f465d
yuseungbeom <ykseok518@naver.com>
2025-03-08 20:01:46 +0900

    ✨ feat: 로그인 및 회원가입 페이지 디자인 개선 및 소셜 로그인 구현
    Tailwind CSS로 로그인 및 회원가입 UI 개선
    소셜 로그인(구글, 카카오) 기능 추가
    django-widget-tweaks 패키지 추가하여 폼 필드 스타일링 개선
    회원가입 폼 UI 개선 및 섹션별 구분
    네비게이션 바에 로그인/회원가입 링크 업데이트

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
     path("accounts/", include("accounts.urls")),
     path("courses/", include("courses.urls")),
     path("", include("courses.urls")),
