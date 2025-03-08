414196ce71ce93235c89d9851cb136cfc88773f2
yuseungbeom <ykseok518@naver.com>
2025-03-08 20:01:46 +0900

    ✨ feat: 로그인 및 회원가입 페이지 디자인 개선 및 소셜 로그인 구현
    Tailwind CSS로 로그인 및 회원가입 UI 개선
    소셜 로그인(구글, 카카오) 기능 추가
    django-widget-tweaks 패키지 추가하여 폼 필드 스타일링 개선
    회원가입 폼 UI 개선 및 섹션별 구분
    네비게이션 바에 로그인/회원가입 링크 업데이트

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,3 +1,4 @@
+
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
@@ -21,7 +22,7 @@ class SignupForm(UserCreationForm):
         widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': '이메일'}),
         required=True
     )
-    
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2', 'first_name', 
