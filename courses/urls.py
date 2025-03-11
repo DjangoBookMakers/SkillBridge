80349543a9a66f4aeb2e61a0ab433744e1e7ccbd
SeungminAhn <smahn9123@gmail.com>
2025-03-11 16:53:34 +0900

    ✨ FEAT: 수강평 수정 및 삭제 기능 추가

--- courses/urls.py
+++ courses/urls.py
@@ -6,6 +6,16 @@ urlpatterns = [
     path("detail/<int:course_id>/", views.course_detail, name="course_detail"),
     path("detail/<int:course_id>/review/", views.add_review, name="course_add_review"),
     path(
+        "review/<int:review_id>/update/",
+        views.update_review,
+        name="course_update_review",
+    ),
+    path(
+        "review/<int:review_id>/delete/",
+        views.delete_review,
+        name="course_delete_review",
+    ),
+    path(
         "lecture/<int:lecture_id>/question/add/",
         views.add_question,
         name="course_add_question",
