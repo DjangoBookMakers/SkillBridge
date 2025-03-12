ee14d8b2629017eb21fc32907969e8ab5a6fdd22
SeungminAhn <smahn9123@gmail.com>
2025-03-12 23:05:53 +0900

    ✨ FEAT: 어드민 대시보드의 프로젝트제출 조회 및 평가 기능 구현

--- admin_portal/urls.py
+++ admin_portal/urls.py
@@ -8,4 +8,17 @@ urlpatterns = [
         views.admin_statistics_api,
         name="admin_portal_statistics_api",
     ),
+    path(
+        "projects/", views.admin_pending_projects, name="admin_portal_pending_projects"
+    ),
+    path(
+        "projects/<int:project_id>/",
+        views.project_detail,
+        name="admin_portal_project_detail",
+    ),
+    path(
+        "projects/<int:project_id>/evaluate/",
+        views.evaluate_project,
+        name="admin_portal_evaluate_project",
+    ),
 ]
