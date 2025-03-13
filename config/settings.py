369ce454e8f24c1c4e764583b3da26966c242e45
SeungminAhn <smahn9123@gmail.com>
2025-03-13 20:49:31 +0900

    Revert "✨ 관리자 대시보드 과정 관리 기능 구현"
    This reverts commit ff057556e90d1ac9c69c2b95bf94ac93a1425e5c.

--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
     "allauth.socialaccount",
     "allauth.socialaccount.providers.google",
     "allauth.socialaccount.providers.kakao",
-    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
