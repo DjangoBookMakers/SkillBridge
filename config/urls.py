48eb64886248884f35dd3ef01d8255e9bca58921
yuseungbeom <ykseok518@naver.com>
2025-03-13 14:12:12 +0900

    🔗 관리자 포털 URL 라우팅 설정

--- config/urls.py
+++ config/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("courses/", include("courses.urls")),
     path("learning/", include("learning.urls")),
+    path("admin_portal/", include("admin_portal.urls")),
     path("", include("courses.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
