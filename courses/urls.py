9ae4f99c005c642f9bcd77c5b068c1b000d26a67
SeungminAhn <smahn9123@gmail.com>
2025-03-18 13:23:53 +0900

    ♻️ REFACTOR: courses 앱에서 app_name 사용

--- courses/urls.py
+++ courses/urls.py
@@ -1,28 +1,22 @@
 from django.urls import path
 from . import views
 
+app_name = "courses"
+
 urlpatterns = [
     path("", views.course_list, name="course_list"),
     path("detail/<int:course_id>/", views.course_detail, name="course_detail"),
-    path("detail/<int:course_id>/review/", views.add_review, name="course_add_review"),
-    path(
-        "review/<int:review_id>/update/",
-        views.update_review,
-        name="course_update_review",
-    ),
-    path(
-        "review/<int:review_id>/delete/",
-        views.delete_review,
-        name="course_delete_review",
-    ),
+    path("detail/<int:course_id>/review/", views.add_review, name="add_review"),
+    path("review/<int:review_id>/update/", views.update_review, name="update_review"),
+    path("review/<int:review_id>/delete/", views.delete_review, name="delete_review"),
     path(
         "lecture/<int:lecture_id>/question/add/",
         views.add_question,
-        name="course_add_question",
+        name="add_question",
     ),
     path(
         "question/<int:question_id>/answer/add/",
         views.add_answer,
-        name="course_add_answer",
+        name="add_answer",
     ),
 ]
